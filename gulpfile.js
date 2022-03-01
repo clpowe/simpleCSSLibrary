68aeabc670632c48db954cd0b5bf4afde871cd35
clpowe <clpowe@gmail.com>
2022-02-28 19:22:16 -0500

    purge css

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,12 @@
 const { src, dest, watch, series } = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
+const purgecss = require('gulp-purgecss')
 
 function buildStyles() {
-	return src('truth/**/*.scss').pipe(sass()).pipe(dest('css'))
+	return src('truth/**/*.scss')
+		.pipe(sass())
+		.pipe(purgecss({ content: ['*.html'] }))
+		.pipe(dest('css'))
 }
 
 function watchTask() {
